194e5b644c127a67cf203f1eb47350e414db9507
Szabó Zoltán <szaboz@inf.u-szeged.hu>
2019-03-28 15:28:15 +0100

    multicasting

--- src/app/observable-example.service.ts
+++ src/app/observable-example.service.ts
@@ -17,7 +17,48 @@ export class ObservableExampleService {
     observer.next("körte");
     observer.next("ananász");
     observer.error("banán");
+    observer.next("kiwi");
     observer.complete();
+
+    return { unsubscribe() {}}
+  }
+
+  sendSomeFruits(observer, arr, idx) {
+    return setTimeout(() => {
+      observer.next(arr[idx]);
+      if(idx === arr.length-1) {
+        observer.complete();
+      } else {
+        this.sendSomeFruits(observer, arr, ++idx);
+      }
+    }, 1000);
+  }
+
+  multicasting() {
+    const gyumolcsLista = ["kiwi", "ananász", "alma", "narancs"];
+    const observers = [];
+    let timeoutId;
+    return (observer) => {
+      observers.push(observer);
+      if(observers.length === 1) {
+        timeoutId = this.sendSomeFruits({
+          next(val) {
+            observers.forEach(obs => obs.next(val));
+          },
+          complete() {
+            observers.slice(0).forEach(obs => obs.complete());
+          }
+        }, gyumolcsLista, 0)
+      }
+      return {
+        unsubscribe() {
+          observers.splice(observers.indexOf(observer),1);
+          if(observers.length === 0) {
+            clearTimeout(timeoutId);
+          }
+        }
+      }
+    }
   }
 
   constructor() { }
@@ -26,6 +67,11 @@ export class ObservableExampleService {
     //this.myObservable.subscribe(this.myObserver);
 
     const gyumolcs = new Observable(this.fruitSeller);
+    gyumolcs.subscribe(data => 
+      console.log("Inline observer adatot kapott", data),
+      error => console.log("Inline observer hibat kapott", error),
+      () => console.log("Inline observer végzett"));
+
     gyumolcs.subscribe(this.myObserver);
   }
 
